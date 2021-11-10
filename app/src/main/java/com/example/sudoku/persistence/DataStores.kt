3bb8865855455b5c8c02709c17e8675d4ac71201
ch <mkmsacc@outlook.com>
2021-11-10 08:23:16 +0700

    Update DataStores

--- app/src/main/java/com/example/sudoku/persistence/DataStores.kt
+++ app/src/main/java/com/example/sudoku/persistence/DataStores.kt
@@ -6,6 +6,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.core.Serializer
 import androidx.datastore.dataStore
 import com.example.sudoku.GameSettings
+import com.example.sudoku.Statistics
 import com.google.protobuf.InvalidProtocolBufferException
 import java.io.InputStream
 import java.io.OutputStream
@@ -16,8 +17,7 @@ internal val Context.settingsDataStore: DataStore<GameSettings> by dataStore(
 )
 
 private object GameSettingsSerializer : Serializer<GameSettings> {
-    override val defaultValue: GameSettings
-        get() = GameSettings.getDefaultInstance()
+    override val defaultValue: GameSettings = GameSettings.getDefaultInstance()
 
     override suspend fun readFrom(input: InputStream): GameSettings {
         try {
@@ -25,8 +25,27 @@ private object GameSettingsSerializer : Serializer<GameSettings> {
         } catch (exception: InvalidProtocolBufferException) {
             throw CorruptionException("Cannot read proto.", exception)
         }
-
     }
 
     override suspend fun writeTo(t: GameSettings, output: OutputStream) = t.writeTo(output)
+}
+
+internal val Context.statsDataStore: DataStore<Statistics> by dataStore(
+    fileName = "user_statistics.pb",
+    serializer = StatisticsSerializer
+)
+
+
+private object StatisticsSerializer : Serializer<Statistics> {
+    override val defaultValue: Statistics = Statistics.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): Statistics {
+        try {
+            return Statistics.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: Statistics, output: OutputStream) = t.writeTo(output)
 }
